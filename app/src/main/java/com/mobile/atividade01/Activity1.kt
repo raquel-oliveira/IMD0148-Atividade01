7463eee78e54d38f333beaf5c2c3f48f18f9fa70
Raquel Oliveira <oliveira.raquel.lopes@gmail.com>
2019-10-10 21:15:31 -0300

    add activities
    add activities with layout defined on specification

--- app/src/main/java/com/mobile/atividade01/Activity1.kt
+++ app/src/main/java/com/mobile/atividade01/Activity1.kt
@@ -3,10 +3,10 @@ package com.mobile.atividade01
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class Activity1 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_1)
     }
 }
