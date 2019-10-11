e39c2618dfefea6317066de67b9cfecf6063aac9
Raquel Oliveira <oliveira.raquel.lopes@gmail.com>
2019-10-10 21:39:43 -0300

    add communication from Activity1 to Activity2

--- app/src/main/java/com/mobile/atividade01/Activity1.kt
+++ app/src/main/java/com/mobile/atividade01/Activity1.kt
@@ -1,12 +1,26 @@
 package com.mobile.atividade01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_1.*
 
 class Activity1 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_1)
+
+        var initialValue = 1;
+        btnCounterAtv1.text = initialValue.toString();
+
+        btnCounterAtv1.setOnClickListener {
+            var value = btnCounterAtv1.text.toString();
+            var i = Intent(this, Activity2::class.java).apply{
+                putExtra("value", value.toInt())
+            }
+            startActivity(i)
+        }
     }
 }
+
--- app/src/main/java/com/mobile/atividade01/Activity2.kt
+++ app/src/main/java/com/mobile/atividade01/Activity2.kt
@@ -2,11 +2,16 @@ package com.mobile.atividade01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_2.*
 
 class Activity2 : AppCompatActivity() {
+    var increment = 1;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
+        var value = intent.getIntExtra("value", 1)
+        btnCounterAtv2.text = (value+increment).toString()
     }
 }
